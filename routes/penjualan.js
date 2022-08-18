278f27be7c6a671b1c389fb891656a2ceb6ca48b
rizkyr123234 <rizkyramdhans17@gmail.com>
2022-08-18 19:37:12 +0700

    menuju add

--- routes/penjualan.js
+++ routes/penjualan.js
@@ -5,8 +5,8 @@ var moment = require('moment')
 const {formatter } = require('../helper/util.js')
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-   //let kode_jualan =kode_jualan=req.query.kode_jualan || cekrow.rows.length > 0 ? cekrow.rows[0].kode_jualan : ''
-   db.query('select detail.barcode, info_barang.nama_barang, detail.jumlah, info_barang.harga_jual, detail.total_harga from detail    join penjualan on penjualan.kode_jualan = detail.kode_jualan join info_barang on detail.barcode = info_barang.barcode ORDER By id_detail')
+   let kode_jualan =req.query.invoice || ''
+   db.query('select detail.barcode, info_barang.nama_barang, detail.jumlah, info_barang.harga_jual, detail.total_harga from detail join penjualan on penjualan.kode_jualan = detail.kode_jualan join info_barang on detail.barcode = info_barang.barcode  where detail.kode_jualan = $1 ORDER By id_detail',[kode_jualan])
   .then(cekcek=>db.query('select * from penjualan')
   .then(result=> res.render('penjualan/list',{item:result.rows,details:cekcek.rows,currentPage:'penjualan', formatter,moment
  })))
